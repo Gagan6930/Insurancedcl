ae88f220b5a926ce82ad2fa3c16c8a9e1459f626
Gagan6930 <gagankumardrgagankumardr@gmail.com>
2026-07-07 12:52:27 +0530

    added connection

---
+++ src/main/java/com/policy/utility/Connector.java
@@ -0,0 +1,19 @@
+package com.policy.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connector {
+	public static Connection requestgetconnection() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Daodemo", "root", "root");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
